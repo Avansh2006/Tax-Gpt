d1e53e262c7f9f9134c6c08444fb715385ec4b4c
Avansh206 <avanshyadav20072006@gmail.com>
2025-02-08 19:25:40 +0530

    commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); 
 
 app.use(bodyParser.json());
 
